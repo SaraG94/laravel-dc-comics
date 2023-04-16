3c40b21aa9796bfc98d804d6377499e3ebf107a2
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-16 23:06:29 +0200

    FEAT content index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.app')
 
 @section('page-title','Ours Comics')
+
 @section('content')
-
   <div class="container">
-    index
-  </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Comic Title</th>
+          <th scope="col">Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($comics as $comic)
 
+        <tr>
+          
+          <td>
+            <p>{{ $comic->title }}</p>
+          </td>
+          <td>
+            <a href="{{ route('comics.show',$comic->id) }}">
+              more info               
+            </a>
+          </td>
+
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title','Ours Comics')
+
+@section('content')
+<div class="container">
+    cucù
+</div>
+@endsection
