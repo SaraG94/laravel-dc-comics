e100d6dc3f49de05a2cdef93945dbed885d18512
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-16 22:11:25 +0200

    FEAT views + content header and footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <header>
+        <div class="container">
+           <h1>DC comics</h1>
+        </div>
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">Home </a>
+                </li>
+                <li>
+                    <a href="#">All comics </a>
+                </li>
+                <li>
+                    <a href="#">Insert a comic</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+
+Route::get('/comics/{comic}',[ComicController::class, 'show'])->name('comics.show');
