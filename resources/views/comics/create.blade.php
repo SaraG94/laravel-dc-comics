59af706b3146c2cccccf361b1e46ddc3134d25b8
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-17 16:04:14 +0200

    FEAT route+contoller store FIX input form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,31 +8,31 @@
   </div>
 
   <div class="container">
-    <form action="">
-    
+    <form action="{{ route('comics.store') }}" method="POST">
+    @csrf
       <div class="form-group">
         <label for="title">Title</label>
-        <input type="email" class="form-control" id="title" name="title" placeholder="Enter Title">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title">
       </div>
       <div class="form-group">
         <label for="thumb">Src image</label>
-        <input type="password" class="form-control" id="thumb" name="thumb" placeholder="https://...">
+        <input type="text" class="form-control" id="thumb" name="thumb" placeholder="https://...">
       </div>
       <div class="form-group">
         <label for="price">Price</label>
-        <input type="password" class="form-control" id="price" name="price" placeholder="$18.99">
+        <input type="text" class="form-control" id="price" name="price" placeholder="$18.99">
       </div>
       <div class="form-group">
         <label for="series">series</label>
-        <input type="password" class="form-control" id="series" name="series" placeholder="series comic">
+        <input type="text" class="form-control" id="series" name="series" placeholder="series comic">
       </div>
       <div class="form-group">
         <label for="sale_date">Sale date</label>
-        <input type="password" class="form-control" id="sale_date" name="sale_date" placeholder="Sale date">
+        <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Sale date">
       </div>
       <div class="form-group">
         <label for="type">Type</label>
-        <input type="password" class="form-control" id="type" name="type" placeholder="type comic">
+        <input type="text" class="form-control" id="type" name="type" placeholder="type comic">
       </div>
       <div class="form-group">
         <label for="description">Description</label>
