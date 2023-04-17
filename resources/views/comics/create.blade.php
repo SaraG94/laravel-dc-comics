ccb55e22b3f40645d7f1c692ffbd5c49633ef308
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-17 15:37:10 +0200

    FEAT content to  form create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,19 +10,35 @@
   <div class="container">
     <form action="">
     
-        <div class="form-group">
-            <label for="exampleInputEmail1">Title</label>
-            <input type="email" class="form-control" id="" placeholder="Enter Title">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Description</label>
-            <input type="password" class="form-control" id="" placeholder="Description">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="email" class="form-control" id="title" name="title" placeholder="Enter Title">
+      </div>
+      <div class="form-group">
+        <label for="thumb">Src image</label>
+        <input type="password" class="form-control" id="thumb" name="thumb" placeholder="https://...">
+      </div>
+      <div class="form-group">
+        <label for="price">Price</label>
+        <input type="password" class="form-control" id="price" name="price" placeholder="$18.99">
+      </div>
+      <div class="form-group">
+        <label for="series">series</label>
+        <input type="password" class="form-control" id="series" name="series" placeholder="series comic">
+      </div>
+      <div class="form-group">
+        <label for="sale_date">Sale date</label>
+        <input type="password" class="form-control" id="sale_date" name="sale_date" placeholder="Sale date">
+      </div>
+      <div class="form-group">
+        <label for="type">Type</label>
+        <input type="password" class="form-control" id="type" name="type" placeholder="type comic">
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea class="form-control" id="description" name="description" placeholder="Description" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Invia</button>
     </form>
   </div>
 
