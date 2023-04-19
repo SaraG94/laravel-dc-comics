80832f90cf420cf2ab9e7461b4e9286dd2e10f62
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-19 15:51:33 +0200

    FEAT old('',$) in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -14,37 +14,37 @@
 
       <div class="form-group">
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="{{ $comic->title }}">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="{{old('title',$comic->title)}}">
       </div>
       <div class="form-group">
         <label for="thumb">Src image</label>
-        <input type="text" class="form-control" id="thumb" name="thumb" placeholder="https://..." value="{{ $comic->thumb }}">
+        <input type="text" class="form-control" id="thumb" name="thumb" placeholder="https://..." value="{{ old('thumb',$comic->thumb)}}">
       </div>
       <div class="form-group">
         <label for="price">Price</label>
-        <input type="text" class="form-control" id="price" name="price" placeholder="$18.99" value="{{ $comic->price }}">
+        <input type="text" class="form-control" id="price" name="price" placeholder="$18.99" value="{{ old('price',$comic->price)}}">
       </div>
       <div class="form-group">
         <label for="series">series</label>
-        <input type="text" class="form-control" id="series" name="series" placeholder="series comic" value="{{ $comic->series}}">
+        <input type="text" class="form-control" id="series" name="series" placeholder="series comic" value="{{ old('series',$comic->series)}}">
       </div>
       <div class="form-group">
         <label for="sale_date">Sale date</label>
-        <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Sale date" value="{{ $comic->sale_date }}">
+        <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Sale date" value="{{ old('sale_date',$comic->sale_date)}}">
       </div>
       <div class="form-group">
         <label for="type">Type</label>
-        <input type="text" class="form-control" id="type" name="type" placeholder="type comic" value="{{ $comic->type }}">
+        <input type="text" class="form-control" id="type" name="type" placeholder="type comic" value="{{ old('type',$comic->type) }}">
       </div>
       <div class="form-group">
         <label for="description">Description</label>
         <textarea class="form-control" id="description" name="description" placeholder="Description" cols="30" rows="10">
-            {{$comic->description}}
+            {{old('description',$comic->description)}}
         </textarea>
       </div>
       <button type="submit" class="btn btn-primary">Salva</button>
     </form>
-    
+
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
